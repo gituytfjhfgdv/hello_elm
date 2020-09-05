008e009db1796cd818df9dbf448491cd2d18698a
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-09-06 02:06:33 +0900

    tutorial call github api

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,151 @@
 module Main exposing (main)
 
-import Html exposing (Html, a, div, h1, li, text, ul)
-import Html.Attributes exposing (href, target)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as D exposing (Decoder)
 
 
-main : Html msg
+main : Program () Model Msg
 main =
-    div [] [ header, content ]
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
 
 
-header : Html msg
-header =
-    h1 [] [ text "Useful Links" ]
+
+-- Model
 
 
-content : Html msg
-content =
-    ul []
-        [ linkItem "https://elm-lang.org" "homepage"
-        , linkItem "https://elm-lang.org" "Elm"
-        , linkItem "https://elm-lang.org" "Elm"
+type alias Model =
+    { input : String
+    , userState : UserState
+    }
+
+
+type UserState
+    = Init
+    | Waiting
+    | Loaded User
+    | Failed Http.Error
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "" Init
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = Input String
+    | Send
+    | Receive (Result Http.Error User)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Input newInput ->
+            ( { model | input = newInput }, Cmd.none )
+
+        Send ->
+            ( { model
+                | input = ""
+                , userState = Waiting
+              }
+            , Http.get
+                { url = "https://api.github.com/users/" ++ model.input
+                , expect = Http.expectJson Receive userDecoder
+                }
+            )
+
+        Receive (Ok user) ->
+            ( { model | userState = Loaded user }, Cmd.none )
+
+        Receive (Err e) ->
+            ( { model | userState = Failed e }, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ Html.form [ onSubmit Send ]
+            [ input
+                [ onInput Input
+                , autofocus True
+                , placeholder "Github name"
+                , value model.input
+                ]
+                []
+            , button
+                [ disabled
+                    ((model.userState == Waiting)
+                        || String.isEmpty (String.trim model.input)
+                    )
+                ]
+                [ text "Submit" ]
+            ]
+        , case model.userState of
+            Init ->
+                text ""
+
+            Waiting ->
+                text "waiting"
+
+            Loaded user ->
+                a
+                    [ href user.htmlUrl
+                    , target "_blank"
+                    ]
+                    [ img [ src user.avatarUrl, width 200 ] []
+                    , div [] [ text user.name ]
+                    , div []
+                        [ case user.bio of
+                            Just bio ->
+                                text bio
+
+                            Nothing ->
+                                text ""
+                        ]
+                    ]
+
+            Failed error ->
+                div [] [ text (Debug.toString error) ]
         ]
 
 
-linkItem : String -> String -> Html msg
-linkItem url text_ =
-    li [] [ a [ href url, target "_blank" ] [ text text_ ] ]
+
+-- Data
+
+
+type alias User =
+    { login : String
+    , avatarUrl : String
+    , name : String
+    , htmlUrl : String
+    , bio : Maybe String
+    }
+
+
+userDecoder : Decoder User
+userDecoder =
+    D.map5 User
+        (D.field "login" D.string)
+        (D.field "avatar_url" D.string)
+        (D.field "name" D.string)
+        (D.field "html_url" D.string)
+        (D.maybe (D.field "bio" D.string))
