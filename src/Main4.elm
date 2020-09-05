dc4f27ae9d437722f20fc34f2b971966b7e1f9ec
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-09-05 19:56:35 +0900

    はじめてのhtml生成

--- src/Main4.elm
+++ src/Main4.elm
@@ -1,9 +1,28 @@
 module Main4 exposing (main)
 
-import Html exposing (Html, a, text)
-import Html.Attributes exposing (href)
+import Html exposing (Html, a, div, h1, li, text, ul)
+import Html.Attributes exposing (href, target)
 
 
 main : Html msg
 main =
-    a [ href "https://elm-lang.org" ] [ text "Elm" ]
+    div [] [ header, content ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ linkItem "https://elm-lang.org" "homepage"
+        , linkItem "https://elm-lang.org" "Elm"
+        , linkItem "https://elm-lang.org" "Elm"
+        ]
+
+
+linkItem : String -> String -> Html msg
+linkItem url text_ =
+    li [] [ a [ href url, target "_blank" ] [ text text_ ] ]
